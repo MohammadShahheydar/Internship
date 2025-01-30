8eb6cf44368b66fe45188fb796b33fbcf25e502d
MohammadShahheydar <m.shahheydar110>
2025-01-30 21:01:08 +0330

    fix: final report path in dto

--- src/main/java/mohammad/shahheydar/internshipprocessmanagement/mapper/InternshipMapper.java
+++ src/main/java/mohammad/shahheydar/internshipprocessmanagement/mapper/InternshipMapper.java
@@ -1,11 +1,22 @@
 package mohammad.shahheydar.internshipprocessmanagement.mapper;
 
 import mohammad.shahheydar.internshipprocessmanagement.entity.Internship;
+import mohammad.shahheydar.internshipprocessmanagement.entity.InternshipForm;
 import mohammad.shahheydar.internshipprocessmanagement.model.InternshipDto;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.NullValueCheckStrategy;
+import mohammad.shahheydar.internshipprocessmanagement.model.InternshipFormDto;
+import org.mapstruct.*;
 
 @Mapper(nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS, componentModel = MappingConstants.ComponentModel.SPRING, uses = {StudentMapper.class, EmployeeMapper.class , WeeklyReportMapper.class , PresenceAndAbsenceMapper.class , InternshipFormMapper.class , OpinionMapper.class})
 public interface InternshipMapper extends BaseMapper<Internship , InternshipDto> {
+    @Mapping(target = "studentFinalReport", source = "studentFinalReport", qualifiedByName = "mapStudentFinalReport")
+    @Override
+    InternshipDto toDto(Internship entity);
+
+    @Named("mapStudentFinalReport")
+    default String mapStudentFinalReport(String studentFinalReport) {
+        if (studentFinalReport == null || studentFinalReport.isEmpty()) {
+            return studentFinalReport;
+        }
+        return "http://localhost:8080/files/" + studentFinalReport;
+    }
 }
